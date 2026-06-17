73446e8767caabbcaeedc4a0969ef9719f9b16be
sniffer <134985415+mmogqsd@users.noreply.github.com>
2026-06-18 09:09:51 +1000

    Updated msg_script.py

--- msg_script.py
+++ msg_script.py
@@ -1,4 +1,7 @@
-#script uses netbios names and a dedicated host port to connect clients with a mesh topology
+#script uses udp broadcasts and a dedicated host port to connect clients with a mesh topology
+
+import struct
+import pickle
 
 #for finding
 import subprocess
@@ -12,19 +15,13 @@ import sys
 import json
 import os
 
-#Olly's:  ANONYMOUS-10
-#Jason's: YEGJK-LAPTOP
-#Perry's: MAC-4B923D
-#Aaron's: GORT
-#Hugo's NWPC
-#Allesio's MAC-42D3AC
-NB_List = ["GORT", "ANONYMOUS-10", "YEGJK-LAPTOP", "MAC-4B923D", "NWPC", "MAC-42D3AC"]
-Search_List = NB_List
 IPs_Found = []
 interval = 5.3
 
-disconnect_code = "CLIENT_DISCONNECT"
+discovery_code = "DISCOVERY_PACKET"
+disconnect_code = "CLIENT_DISCONNECT" 
 
+server_sock = None
 port = 5630
 incremented_port = 5631
 CONN_LIST = []
@@ -32,7 +29,63 @@ debug = True
 
 
 
+mcast_ip = "224.0.0.251"
+mcast_group = '224.1.1.1'
+upd_port = 56302
+ttl = 10
 
+data_list = []
+
+class packet:
+    def __init__(self, conn_port: str, name: str, type: str):
+        self.port = conn_port
+        self.name = name
+        self.type = type
+
+
+udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+udp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+udp_sock.bind(('', upd_port))
+mreq = struct.pack("4si", socket.inet_aton(mcast_group), socket.INADDR_ANY)
+udp_sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+
+#gets your ip to prevent self connections
+def get_ip():
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.connect(("8.8.8.8", 80))
+        ip_address = s.getsockname()[0]
+        return ip_address
+    
+self_ip = get_ip()
+
+#closes
+def stop_server():
+    global server_sock
+    if server_sock:
+        print(server_sock)
+        server_sock.close()
+        print(server_sock)
+
+#udp listening on multicast group
+def listen_udp(prompt, name):
+    while True:
+        data, address = udp_sock.recvfrom(1024)
+        address = address[0]
+        if address and address not in IPs_Found and address[0] != self_ip:
+            data = pickle.loads(data)
+            #print(f"\n[|] client data receievd from {address}: {data}")
+            threading.Thread(target=connect, args=(address, data, prompt), daemon=True).start()
+
+#sends packet over udp multicast group
+def send_udp(prompt, name, packet):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+
+    message = pickle.dumps(packet)
+
+    #print(f"\nSending packet to group {mcast_group}")
+    sock.sendto(message, (mcast_group, upd_port))
 
 
 #thread for receiving data
@@ -40,6 +93,7 @@ def receive(sock, nameO, prompt):
     while True:
         try:
             data = sock.recv(1024).decode()
+            print("data rec")
             if not data:
                 break
             if data == disconnect_code:
@@ -78,6 +132,9 @@ def receive(sock, nameO, prompt):
 
     
 def close_sockets():
+    global server_sock
+    udp_sock.close()
+    server_sock.close()
     for sock in CONN_LIST:
         try:
             CONN_LIST.remove(sock)
@@ -97,28 +154,92 @@ def send(prompt):
                 except:
                     pass
                 close_sockets()
-            sys.exit(0)
+            sys.exit()
+
 
         for sock in CONN_LIST:
             try:
                 sock.send(msg.encode())
             except:
                 pass
+    
 
+#starts the mesh topo connecting
+def connect(address, data, prompt):
+    
+    
+    name = data.name
+    port = data.port
+    for active_sock in CONN_LIST:
+        try:
+            if active_sock.getpeername()[0] == address:
+                if debug:
+                    sys.stdout.write("\r\033[K")
+                    sys.stdout.write(f"[=] Not allowing connection to {name} because there is already an active connection")
+                    sys.stdout.flush()
+                    readline.redisplay()
+                return False
+        except:
+            pass
 
+    #sys.stdout.write(f"\nconnecting to: {address}\n")
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(4)
+        sock.connect((address, port))
+        
+        server_response = sock.recv(1024).decode()
+        if server_response == "ALREADY_CONNECTED":
+            return False
+        sock.close() 
+        
+        if server_response.startswith("SHIFT_PORT:"):
+            routed_port = int(server_response.split(":")[1])
+            
+            time.sleep(0.5) 
+            
+            dedicated_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            dedicated_sock.connect((address, routed_port))
+            
+            nameO = dedicated_sock.recv(1024).decode()
+            dedicated_sock.send(name.encode())
+            
+            CONN_LIST.append(dedicated_sock)
+            sys.stdout.write("\r\033[K")
+            sys.stdout.write(f"[+] connected to {name} on shifted port {routed_port}!")
 
+            sys.stdout.flush()
+            readline.redisplay()
 
+            threading.Thread(target=receive, args=(dedicated_sock, nameO, prompt), daemon=True).start()
+            
+    except Exception as e:
+        print(f"[-] Could not connect or route to {name} at {address}")
+        sys.stdout.flush()
+        readline.redisplay()
 
 #thread for initialising and maintaining server of host
 def server(prompt, name): 
-    global incremented_port
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(("0.0.0.0", port))
-    sock.listen(5)
-    print(f"-Server is listening on {port}-")
+    global incremented_port, server_sock
+    server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    server_sock.bind(("0.0.0.0", port))
+    
+    server_sock.listen(5)
+    sys.stdout.write("\r\033[K")
+    sys.stdout.write(f"-Server is listening on {port}-\n")
+    sys.stdout.write("\r\033[K")
+    sys.stdout.flush()
+    readline.redisplay()
+
     while True:
         try:
-            gateway_conn, address = sock.accept()
+            gateway_conn, address = server_sock.accept()
+
+            if address[0] == self_ip:
+                gateway_conn.send("ALREADY_CONNECTED".encode())
+                gateway_conn.close()
+                return
 
 
             already_connected = False
@@ -155,10 +276,14 @@ def server(prompt, name):
                     
                     CONN_LIST.append(conn)
                     print(f"\n[+] Peer successfully routed and established on dedicated port {target_port}")
-                    
+                    print("receieve called from server")
                     threading.Thread(target=receive, args=(conn, nameO, prompt), daemon=True).start()
                     dedicated_sock.close() 
                 except:
+                    try:
+                        dedicated_sock.close()
+                    except:
+                        print("damn")
                     pass
 
             threading.Thread(target=dedicated_listener, args=(given_port,), daemon=True).start()
@@ -166,95 +291,9 @@ def server(prompt, name):
         except Exception as e:
             pass
 
-#finds the ips of specified netbios names (this vers has olly, jason, perry, hugo, and my NetBios name)
-def find():
-    NB_List_Copy = list(Search_List) 
-    for NB in NB_List_Copy:
-        if debug:
-            print(f"Searching for {NB}")
-        try:
-            output = subprocess.run(
-                ["nmblookup", NB],
-                capture_output=True,
-                text=True,
-                check=True,
-                timeout=5
-            )
-            if debug:
-                print(f"{NB} Found:")
-            i = output.stdout.split()[0]
-            if debug:
-                print(i)
-            if NB in Search_List:
-                Search_List.remove(NB)
-            
-        except subprocess.CalledProcessError as e:
-            if debug:
-                print()
-                print(f"Failed to find IP of {NB}: Most likely, target computer is 'asleep'")
-                print()
-            i = "C"
-        
-        if i == "C":
-            i = "C Unknown"
-        else:
-            i = i + " " + NB
-            
-        IPs_Found.append(i)
-    
-    
-#starts the mesh topo connecting
-def connect(ip_element, name, prompt):
-    ip_address = ip_element.split()[0]
-    nb_name = ip_element.split()[1]
-
-    if ip_address == "C": 
-        return False
-
-    for active_sock in CONN_LIST:
-        try:
-            if active_sock.getpeername()[0] == ip_address:
-                if debug:
-                    print(f"[=] Not allowing connection to {nb_name} because there is already an active connection")
-                return False
-        except:
-            pass
-
-    print(f"\nconnecting to: {nb_name}, {ip_address}\n")
-    try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(4)
-        sock.connect((ip_address, port))
-        
-        server_response = sock.recv(1024).decode()
-        if server_response == "ALREADY_CONNECTED":
-            return False
-        sock.close() 
-        
-        if server_response.startswith("SHIFT_PORT:"):
-            routed_port = int(server_response.split(":")[1])
-            
-            time.sleep(0.5) 
-            
-            dedicated_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            dedicated_sock.connect((ip_address, routed_port))
-            
-            nameO = dedicated_sock.recv(1024).decode()
-            dedicated_sock.send(name.encode())
-            
-            CONN_LIST.append(dedicated_sock)
-            print(f"[+] connected to {nb_name} on shifted port {routed_port}!")
-
-            
-            threading.Thread(target=receive, args=(dedicated_sock, nameO, prompt), daemon=True).start()
-            
-    except Exception as e:
-        print(f"[-] Could not connect or route to {nb_name} at {ip_address}")
-
-
 
 def main():
-    global port, incremented_port, interval, NB_List, Search_List, debug
+    global port, incremented_port, interval, debug
     
     if os.path.exists("config.json"):
         try:
@@ -263,8 +302,6 @@ def main():
                 port = saved_data.get("port", port)
                 incremented_port = saved_data.get("incremented_port", incremented_port)
                 interval = saved_data.get("interval", interval)
-                NB_List = saved_data.get("NB_List", NB_List)
-                Search_List = NB_List
                 debug = saved_data.get("debug", debug)
         except:
             pass
@@ -272,7 +309,11 @@ def main():
     print("Type start to 'start' or 'help' for commands")
 
     while True:
-        user_cmd = input("setup> ").strip()
+        try:
+            user_cmd = input("setup> ").strip()
+        except KeyboardInterrupt:
+            close_sockets()
+            sys.exit()
         
         if user_cmd == "start":
             break
@@ -323,7 +364,6 @@ def main():
         "port": port,
         "incremented_port": incremented_port,
         "interval": interval,
-        "NB_List": NB_List,
         "debug": debug
     }
     with open("config.json", "w") as f:
@@ -332,14 +372,14 @@ def main():
     name = input("display as: ")
     prompt = f"> "
     
-    threading.Thread(target=server, args=(prompt, name), daemon=True).start()
-
-    for i in range(10):
-        find()
-
-    for ip in IPs_Found:
-        connect(ip, name, prompt)
     
+    upd_packet = packet(port, name, discovery_code)
+    threading.Thread(target=listen_udp, args=(prompt, name), daemon=True).start()
+    threading.Thread(target=server, args=(prompt, name), daemon=True).start()
+    threading.Thread(target=send_udp, args=(prompt, name, upd_packet), daemon=True).start()
+
+
+
     send(prompt)
     
 
